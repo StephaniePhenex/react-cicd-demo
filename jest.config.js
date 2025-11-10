202cd774b52bb425dfc4a913716a35a415453824
sillyfunnypedro <sillyfunnypedro@gmail.com>
2025-11-09 20:05:41 -0800

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+};
+
